e2f26202960d080359259fccafd9d52b6d5eef54
keuwey <keuwey@gmail.com>
2024-12-02 13:42:56 -0300

    fix: improved code reusability

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ from menu import exibir_menu
 pygame.mixer.init()
 
 
+def reproducao(audio_file: str, start: float = 0.0) -> None:
+    """Carrega e inicia a reprodução de um áudio a partir de um ponto específico."""
+    pygame.mixer.music.load(audio_file)
+    pygame.mixer.music.play()
+    pygame.mixer.music.set_pos(start)
+
+
 def reproduzir_letra(audio_file: str) -> None:
     """Reproduz o áudio e sincroniza com a exibição da letra no terminal"""
     try:
         escolha = exibir_menu()
         if escolha == "1":
-            pygame.mixer.music.load(audio_file)
-            pygame.mixer.music.play()
+            reproducao(audio_file, start=0)
             time.sleep(21)
             exibir_letra_com_delay(parte_erguei_as_maos(1), delay=1.6)
             parte_animaizinhos()
         elif escolha == "2":
-            pygame.mixer.music.load(audio_file)
-            pygame.mixer.music.play(start=21)
+            reproducao(audio_file, start=21)
             exibir_letra_com_delay(parte_erguei_as_maos(1), delay=1.6)
 
     except pygame.error as e:
