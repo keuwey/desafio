3763f08ec16ea3a394f4ee9fa13c9fe0d5512aea
keuwey <keuwey@gmail.com>
2024-12-02 13:10:12 -0300

    fix: small improvement in the logic

--- main.py
+++ main.py
@@ -10,26 +10,21 @@ pygame.mixer.init()
 
 def reproduzir_letra(audio_file: str) -> None:
     """Reproduz o áudio e sincroniza com a exibição da letra no terminal"""
-    escolha = exibir_menu()
-    if escolha == "1":
-        parte_erguei_as_maos()
-        parte_animaizinhos()
-        parte_animaizinhos()
-
     try:
-        pygame.mixer.music.load(audio_file)
-        pygame.mixer.music.play()
-        time.sleep(20)
+        escolha = exibir_menu()
+        if escolha == "1":
+            pygame.mixer.music.load(audio_file)
+            pygame.mixer.music.play()
+            time.sleep(21)
+            exibir_letra_com_delay(parte_erguei_as_maos(1), delay=1.6)
+            parte_animaizinhos()
+        elif escolha == "2":
+            pygame.mixer.music.load(audio_file)
+            pygame.mixer.music.play(start=21)
+            exibir_letra_com_delay(parte_erguei_as_maos(1), delay=1.6)
 
-        exibir_letra_com_delay(parte_erguei_as_maos(1), delay=1.6)
-        pygame.mixer.music.stop()
     except pygame.error as e:
         print(f"Erro ao carregar ou reproduzir o áudio: {e}")
 
 
-# if __name__ == "__main__":
-#     while True:
-#         opcao = exibir_menu()
-#         if opcao == "4":
-#             break
 reproduzir_letra("erguei_as_maos.mp3")
--- menu.py
+++ menu.py
@@ -7,5 +7,5 @@ def exibir_menu() -> str:
     print("2 - Reproduzir a parte 'Erguei as Mãos'")
     print("3 - Reproduzir a parte 'O Senhor tem Muitos Filhos'")
     print("4 - Sair")
-    escolha: str = input("Escolha uma opção: ").strip()
+    escolha: str = input("\nEscolha uma opção: ").strip()
     return escolha
