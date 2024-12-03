4868ac373fe06d0bd0ef574ef0efdaee4aa5df57
keuwey <keuwey@gmail.com>
2024-12-03 15:57:29 -0300

    fix: finishing the whole lyrics and parts of it

--- lyrics.py
+++ lyrics.py
@@ -13,19 +13,30 @@ def parte_erguei_as_maos(repeticoes: int = 1) -> list[str]:
     return trecho * repeticoes
 
 
-def parte_animaizinhos(repeticoes: int = 1) -> list[str]:
-    """Retorna o trecho da parte 'Os animaizinhos subiram de dois em dois'."""
+def parte_animaizinhos(animal1: str, animal2: str, repeticoes: int = 1) -> list[str]:
+    """
+    Retorna o trecho da parte 'Os animaizinhos subiram de dois em dois'.
+    Parameters:
+        repeticoes: Número de vezes que o verso irá se repetir
+        animal1: Nome do primeiro animal para substituir
+        animal1: Nome do primeiro animal para substituir
+    """
     base: list[str] = [
         "Os animaizinhos subiram de dois em dois",
         "Os animaizinhos subiram de dois em dois",
-        "O elefante",
-        "E os passarinhos, como os filhos do Senhor",
+        f"{animal1}",
+        f"E {animal2}, como os filhos do Senhor",
         "\n",
     ]
     return base * repeticoes
 
 
-def parte_constroi_arca(repeticoes: int = 1):
+def parte_constroi_arca(repeticoes: int = 1) -> list[str]:
+    """
+    Retorna o trecho da parte 'Deus disse a Noé'.
+    Parameters:
+        repeticoes: Número de vezes que o verso irá se repetir
+    """
     base: list[str] = [
         "Deus disse a Noé: Constrói uma arca",
         "Deus disse a Noé: Constrói uma arca",
@@ -41,11 +52,15 @@ def por_isso() -> list[str]:
 
 
 def separador() -> list[str]:
-    return ["E atenção agora, porque"]
+    return ["E atenção agora, porque", "\n"]
 
 
 def parte_braços_movimentos(repeticoes: int = 1) -> list[str]:
-    """Retorna o trecho da parte 'O Senhor tem muitos filhos'."""
+    """
+    Retorna o trecho da parte 'O Senhor tem muitos filhos'.
+    Parameters:
+        repeticoes: Número de vezes que o verso irá se repetir
+    """
     base: list[str] = [
         "O Senhor tem muitos filhos",
         "Muitos filhos ele tem",
@@ -59,8 +74,9 @@ def parte_braços_movimentos(repeticoes: int = 1) -> list[str]:
 def exibir_letra_com_delay(letra: list[str], delay: float | int) -> None:
     """
     Exibe cada linha da letra no terminal com um intervalo de tempo (delay).
-    :param letra: Lista de strings representando a letra.
-    :param delay: Tempo em segundos entre cada linha.
+    Parameters:
+        `letra`: Lista de strings representando a letra.
+        `delay`: Tempo em segundos entre cada linha.
     """
     for linha in letra:
         print(linha)
--- main.py
+++ main.py
@@ -36,28 +36,54 @@ def reproducao(audio_file: str, start: float, end: float = 296.0) -> None:
 
 def reproduzir_letra(audio_file: str) -> None:
     """Reproduz o áudio e sincroniza com a exibição da letra no terminal"""
+
+    animais = [
+        ("O elefante", "os passarinhos"),
+        ("A minhoquinha", "os pinguins"),
+        ("O canguru", "o sapinho"),
+    ]
     try:
         escolha = exibir_menu()
         if escolha == "1":
             exibir_letra_com_delay(parte_erguei_as_maos(), DELAY)
-            exibir_letra_com_delay(parte_animaizinhos(3), DELAY)
+            for animal1, animal2 in animais:
+                letra = parte_animaizinhos(animal1, animal2)
+                exibir_letra_com_delay(letra, DELAY)
+            # exibir_letra_com_delay(parte_animaizinhos(3), DELAY)
             exibir_letra_com_delay(parte_constroi_arca(), DELAY)
             exibir_letra_com_delay(por_isso(), DELAY)
             exibir_letra_com_delay(parte_erguei_as_maos(3), DELAY)
             exibir_letra_com_delay(separador(), DELAY)
             exibir_letra_com_delay(parte_braços_movimentos(), DELAY)
-            print(MOVIMENTOS[0])
+            print("%s\n" % MOVIMENTOS[0])
             exibir_letra_com_delay(parte_braços_movimentos(), DELAY)
-            print("%s, %s" % (MOVIMENTOS[0], MOVIMENTOS[1]))
+            print("%s, %s\n" % (MOVIMENTOS[0], MOVIMENTOS[1]))
             exibir_letra_com_delay(parte_braços_movimentos(), DELAY)
             print("%s, %s\n%s\n" % (MOVIMENTOS[0], MOVIMENTOS[1], MOVIMENTOS[2]))
             exibir_letra_com_delay(parte_braços_movimentos(), DELAY)
             print("%s, %s\n%s, %s\n" % (MOVIMENTOS[0], MOVIMENTOS[1], MOVIMENTOS[2], MOVIMENTOS[3]))
-            print(MOVIMENTOS[4])
-            exibir_letra_com_delay(parte_erguei_as_maos(), DELAY)
-            print("%s, %s\n%s, %s" % (MOVIMENTOS[0], MOVIMENTOS[1], MOVIMENTOS[2], MOVIMENTOS[3]))
-            print("%s, %s\n%s\n" % (MOVIMENTOS[4], MOVIMENTOS[5], MOVIMENTOS[6]))
             exibir_letra_com_delay(parte_braços_movimentos(), DELAY)
+            print("%s, %s\n%s, %s\n" % (MOVIMENTOS[0], MOVIMENTOS[1], MOVIMENTOS[2], MOVIMENTOS[3]))
+            print("%s" % MOVIMENTOS[4])
+            exibir_letra_com_delay(parte_braços_movimentos(), DELAY)
+            print("%s, %s\n%s, %s\n" % (MOVIMENTOS[0], MOVIMENTOS[1], MOVIMENTOS[2], MOVIMENTOS[3]))
+            print("%s, %s" % (MOVIMENTOS[4], MOVIMENTOS[5]))
+            exibir_letra_com_delay(parte_braços_movimentos(), DELAY)
+
+            print(
+                "%s, %s\n%s, %s\n%s, %s\n%s\n"
+                % (
+                    MOVIMENTOS[0],
+                    MOVIMENTOS[1],
+                    MOVIMENTOS[2],
+                    MOVIMENTOS[3],
+                    MOVIMENTOS[4],
+                    MOVIMENTOS[5],
+                    MOVIMENTOS[6],
+                )
+            )
+            exibir_letra_com_delay(parte_braços_movimentos(), DELAY)
+
             print(
                 "%s, %s\n%s, %s\n%s, %s\n%s e %s\n"
                 % (
@@ -73,19 +99,15 @@ def reproduzir_letra(audio_file: str) -> None:
             )
 
             reproducao(audio_file, start=0.0)
-            # time.sleep(21)
-            # FIXME: Trocar dinamicamente o nome dos animais
             pygame.mixer.music.stop()
             pygame.mixer.music.unload()
         elif escolha == "2":
-            # FIXME: Por causa do time.sleep(duracao) na linha 21,
-            #       a letra só começa a tocar depois dessa duração
             exibir_letra_com_delay(parte_erguei_as_maos(), DELAY)
             reproducao(audio_file, start=21.7, end=37.0)
             pygame.mixer.music.unload()
         elif escolha == "3":
             exibir_letra_com_delay(parte_constroi_arca(), DELAY)
-            reproducao(audio_file, start=80.0, end=93.0)
+            reproducao(audio_file, start=80.0, end=94.0)
 
     except pygame.error as e:
         print(f"Erro ao carregar ou reproduzir o áudio: {e}")
